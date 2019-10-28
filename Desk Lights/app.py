2106ad860f11e2997474c8643796e6bb5f8db18d
doubleplusthink <2014mustang1@gmail.com>
2019-10-27 18:21:34 -0600

    Added Pulse Functionality and Common Cathode Support

--- Desk Lights/app.py
+++ Desk Lights/app.py
@@ -24,7 +24,7 @@ if currentOs == "linux":
     if len(feedback) > 11:
         port = feedback
 
-elif currentOs == "windows": 
+elif currentOs == "windows":
     import serial.tools.list_ports
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
@@ -32,7 +32,6 @@ elif currentOs == "windows":
         if "Arduino" in p:
             port = p.split(' ', 1)[0]
             break
-    print(port)
 
 board=Arduino(port)
 
@@ -41,6 +40,8 @@ red = board.get_pin('d:3:p')
 green = board.get_pin('d:5:p')
 blue = board.get_pin('d:6:p')
 
+commonAnode = True # set this to false for common cathode setup
+
 theloop = ''
 loopIncrementor = 0
 
@@ -48,20 +49,35 @@ loopIncrementor = 0
 eel.init('web')
 
 def hexToRgb(hex):
-    hex = hex.lstrip('#')
+    hex = str(hex).lstrip('#')
     hlen = len(hex)
-    return( tuple(int(hex[i:i+2], 16) for i in (0, 2, 4)))
+    return(tuple(int(hex[i:i+2], 16) for i in (0, 2, 4)))
 
 def writeRgb(r,g,b):
-    red.write(1-r)
-    green.write(1-g)
-    blue.write(1-b)
+    if commonAnode:
+        r = 1 - r
+        g = 1 - g
+        b = 1 - b
+    red.write(r)
+    green.write(g)
+    blue.write(b)
 
 def writeHex(hex):
     myhex = hexToRgb(hex)
     writeRgb(myhex[0]/255,myhex[1]/255,myhex[2]/255)
 
-writeRgb(0,0,0)
+    #Turn off LEDs to begin with
+if commonAnode:
+    writeRgb(0,0,0)
+else:
+    writeRgb(1,1,1)
+
+def getSteps(hex):
+    rgb = list(hexToRgb(hex))
+    for i in range(3):
+        rgb.append(rgb[0]/255/255)
+        rgb.pop(0)
+    return(rgb)
 
 @eel.expose
 def solid(color):
@@ -83,6 +99,20 @@ def fade(colors):
     theloop = lightLoop(loopIncrementor)
     theloop.fade(colors)
 
+@eel.expose
+def lightning(color):
+    global loopIncrementor
+    loopIncrementor += 1
+    theloop = lightLoop(loopIncrementor)
+    theloop.lightning(color)
+
+@eel.expose
+def neon(color):
+    global loopIncrementor
+    loopIncrementor += 1
+    theloop = lightLoop(loopIncrementor)
+    theloop.neon(color)
+
 class lightLoop:
     def __init__(self, name):
         self.name = name
@@ -91,11 +121,46 @@ class lightLoop:
     def pulse(self, colors):
         while self.running:
             for c in colors:
-                if self.name < loopIncrementor:
-                    self.running = False
-                if self.running:
-                    writeHex(c)
-                    eel.sleep(0.7)
+                toWrite = [0,0,0]
+                increasing = True
+                steps = getSteps(c)
+                pulseIncrementor = 0
+
+                while (increasing == True):
+                    for i in range(3):
+                        toWrite[i] = toWrite[i] + steps[i]
+                        if toWrite[i] > 255:
+                            toWrite[i] = 255
+                    pulseIncrementor += 1
+                    if self.name < loopIncrementor:
+                        self.running = False
+                    if self.running == True:
+                        writeRgb(toWrite[0],toWrite[1],toWrite[2])
+                        eel.sleep(0.01)
+                    else:pass
+                    if pulseIncrementor >= 255:
+                        eel.sleep(1.0)
+                        increasing = False
+
+                while increasing == False:
+                    for i in range(3):
+                        toWrite[i] = toWrite[i] - steps[i]
+                        if toWrite[i] <= 0:
+                            toWrite[i] = 0
+                    pulseIncrementor -= 1
+                    if self.name < loopIncrementor:
+                        self.running = False
+                    if self.running == True:
+                        writeRgb(toWrite[0],toWrite[1],toWrite[2])
+                        eel.sleep(0.01)
+                    else: pass
+                    if pulseIncrementor <= 0:
+                        increasing = True
+
+
+
+
+
 
     def fade(self, colors):
         while self.running:
@@ -105,5 +170,19 @@ class lightLoop:
                 if self.running:
                     writeHex(c)
                     eel.sleep(0.7)
+
+    def lightning(self, color):
+        while self.running:
+            if self.name < loopIncrementor:
+                self.running = False
+            if self.running:
+                writeHex(color)
+
+    def neon(self, color):
+        while self.running:
+            if self.name < loopIncrementor:
+                self.running = False
+            if self.running:
+                writeHex(color)
 
 eel.start('main.html')
